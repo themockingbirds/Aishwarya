a4ff7b6ce1982eddd121729d996d0ffb8be83700
Ujjawal Singh <93848065+themockingbirds@users.noreply.github.com>
2022-05-22 20:30:14 +0530

    Update ExpHandling.java

--- OnlineTuts/Exception Handling/ExpHandling.java
+++ OnlineTuts/Exception Handling/ExpHandling.java
@@ -1,15 +1,16 @@
 class ExpHandling{
 	public static void main(String arg[]){
 		int a=10, b=5 , c=5,x,y;
-
-		x=a/(b-c);
-		System.out.println("x: "+x);
+		
+		// x=a/(b-c); //cause  an error because  dividing by zero is not possible.
+		//System.out.println("x: "+x); //  due to that program will get terminated
+		
 		try{
 			x=a/(b+c);
 			System.out.println("x: "+x);
 		}
 		catch(ArithmeticException e){
-			System.out.println("Error Ouccred : Division By 0");
+			System.out.println("Error Occurred : Division By 0");
 		}
 		
 		y=a/(b+c);
